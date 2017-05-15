8f361e0b2dfef5029eaa01484c33f0e152b15345
Tijs <slaats@di.ku.dk>
2017-05-15 17:25:06 +0300

    Test cleanup

--- Reseda.Tests/ParserTests.cs
+++ Reseda.Tests/ParserTests.cs
@@ -122,72 +122,42 @@ namespace Reseda.Tests
 
 
         [TestMethod]
-        public void CurrentHackingTest2()
+        public void FilterTest()
         {
-            string input = "A<?>," +
-                           "B<?>," +
-                           "C<@A:v + @B:v * 10>," +
-                           "D<!>," +
-                           "E<!>" +
-                           "; A -->* B[1]," +
-                           " D -->> {N<?>;}," +
-                           "E -->% *";
+            string input = "A<!>," +
+                           "B<!>," +
+                           "B<!>," +
+                           "C<!>" +
+                           "; A -->* B[1]";
+
             var p = new ResedaParser();
-            p.dispTree(input);
+            p.dispTree(input);          
 
-            //System.Diagnostics.Debug.WriteLine(p.Generate(input).PrintTree());
-
-            var term = p.Generate(input);
-
-            System.Diagnostics.Debug.WriteLine(term.subProcess.ToSource());
-
-            //var term2 = term.CloneJson();
+            var term = p.Generate(input);            
             var term2 = p.Generate(term.subProcess.ToSource());
 
-            InputEvent a = (InputEvent)term.subProcess.structuredData[0];
-            InputEvent b = (InputEvent)term.subProcess.structuredData[1];
-            OutputEvent c = (OutputEvent)term.subProcess.structuredData[2];
-            OutputEvent d = (OutputEvent)term.subProcess.structuredData[3];
-            OutputEvent e = (OutputEvent)term.subProcess.structuredData[4];
+            Assert.AreEqual(term.ToSource(), term2.ToSource());
 
-            a.Execute(1);
+
+
+            OutputEvent a = (OutputEvent)term.subProcess.structuredData[0];
+            OutputEvent b0 = (OutputEvent)term.subProcess.structuredData[1];
+            OutputEvent b1 = (OutputEvent)term.subProcess.structuredData[2];
+            OutputEvent c = (OutputEvent)term.subProcess.structuredData[3];
+            
+
+            b0.Execute();
             System.Diagnostics.Debug.WriteLine(term.PrintTree(true));
 
-            b.Execute(5);
-            System.Diagnostics.Debug.WriteLine(term.PrintTree(true));
+            try
+            {
+                b1.Execute();
+                Assert.Fail();
+            }
+            catch
+            { }
 
-            c.Execute();
-            System.Diagnostics.Debug.WriteLine(term.PrintTree(true));
-
-            d.Execute();
-            System.Diagnostics.Debug.WriteLine(term.PrintTree(true));
-
-            d.Execute();
-            System.Diagnostics.Debug.WriteLine(term.PrintTree(true));
-
-            System.Diagnostics.Debug.WriteLine("TERM 2");
-
-            a = (InputEvent)term2.subProcess.structuredData[0];
-            b = (InputEvent)term2.subProcess.structuredData[1];
-            c = (OutputEvent)term2.subProcess.structuredData[2];
-            d = (OutputEvent)term2.subProcess.structuredData[3];
-            e = (OutputEvent)term2.subProcess.structuredData[4];
-
-            System.Diagnostics.Debug.WriteLine(term2.PrintTree(true));
-            a.Execute(1);
-            System.Diagnostics.Debug.WriteLine(term2.PrintTree(true));
-
-            b.Execute(5);
-            System.Diagnostics.Debug.WriteLine(term2.PrintTree(true));
-
-            c.Execute();
-            System.Diagnostics.Debug.WriteLine(term2.PrintTree(true));
-
-            d.Execute();
-            System.Diagnostics.Debug.WriteLine(term2.PrintTree(true));
-
-            d.Execute();
-            System.Diagnostics.Debug.WriteLine(term2.PrintTree(true));
+            System.Diagnostics.Debug.WriteLine(term.PrintTree(true));           
 
         }
 
