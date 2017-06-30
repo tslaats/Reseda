33ce79e4fbfdaf44496e3a180ac8a5d0d3716340
Tijs <slaats@di.ku.dk>
2017-07-01 00:00:37 +0200

    example 2

--- Reseda.Tests/PaperTests.cs
+++ Reseda.Tests/PaperTests.cs
@@ -23,5 +23,35 @@ invoiceRow[@name:value + ';' + @price:value + ';' + @quantity:value + ';' + @amo
             var term2 = p.Generate(term.subProcess.ToSource());
             Assert.AreEqual(term.ToSource(), term2.ToSource());
         }
+
+
+        [TestMethod]
+        public void Example2()
+        {
+            string input = @"!name[?],
+!price[?],
+!quantity[?],
+amount[@price:value * @quantity:value],
+invoiceRow[@name:value + ';' + @price:value + ';' + @quantity:value + ';' + @amount:value],
+checkout[?],
+ship[?]
+~
+name --><> checkout,
+price --><> checkout,
+quantity --><> checkout, 
+checkout -->% name,
+checkout -->% price,
+checkout -->% quantity,
+checkout -->* ship,
+checkout*--> ship,
+ship -->% checkout
+";
+            var p = new ResedaParser();
+            p.dispTree(input);
+            var term = p.Generate(input);
+            System.Diagnostics.Debug.WriteLine(term.subProcess.ToSource());
+            var term2 = p.Generate(term.subProcess.ToSource());
+            Assert.AreEqual(term.ToSource(), term2.ToSource());
+        }
     }
 }
